16ec242a4cb721f3939be7c56be8b2e1baca362f
CSarte <csarte250@gmail.com>
2024-09-11 10:20:36 -0400

    added base project description

--- face_recognition.py
+++ face_recognition.py
@@ -1,4 +1,17 @@
 # face_recognition.py
+"""Project Description
+
+This project will work to build a PCA with k-means clustering to predict how compeitors wil perform in 
+the game survivor based on a face-recogniton model. Who's face is most similar to a survivor host and 
+there for may become one? 
+
+Goal:
+Face recognition is a classic real-world application for **_unsupervised learning_** techniques. 
+In this assignment, you will work in pairs to develop Python code that leverages principal component
+analysis (PCA) and k-means clustering along with supervised learning methods such as nearest neighbors
+and neural networks to explore an image dataset related to the reality TV show _Survivor_.
+
+"""
 # Add some comments here about what the code does.
 # raghr
 
